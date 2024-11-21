49959009d48c7d20077445051db2744e9c569f18
Abell112233 <abelllfineghan@gmail.com>
2024-11-21 11:27:35 -0300

    pagina inicial

--- main/urls.py
+++ main/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('inicial/', views.inicial, name='inicial'),
 ]
 
 if settings.DEBUG:
